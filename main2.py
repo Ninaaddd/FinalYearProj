c53567dee009b72e37167cca54cea6c5eb9281ef
Ninad Kulkarni <118683630+Ninaaddd@users.noreply.github.com>
2024-04-03 21:33:17 +0530

    Updated main2.py

--- main2.py
+++ main2.py
@@ -13,6 +13,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import pyowm
+import geocoder
 from newsapi import NewsApiClient
 import vertexai
 from vertexai.language_models import TextGenerationModel
@@ -20,6 +21,7 @@ from vertexai.language_models import TextGenerationModel
 # Initialize API
 owm = pyowm.OWM('ea1c27935c886d9b6b465e2f23a44b0c')
 newsapi = NewsApiClient(api_key='b8a263fc824049a692f4aed478f1bd96')
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "D:\\Ninad\\SeventhProject\\seventhproject-418912-84e59efd3a72.json"
 
 # Initialize BERT tokenizer and model
 
@@ -40,7 +42,7 @@ class IntentModel(nn.Module):
 
 
 model = IntentModel(
-    BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=6))
+    BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=7))
 model.load_state_dict(model_state_dict)
 
 
@@ -67,14 +69,15 @@ class RNNModel(nn.Module):
 # defining methods
 
 
-def get_weather(location):
+def get_weather():
     try:
-        location = str(location)
-        observation = owm.weather_manager().weather_at_place(location)
+        location = geocoder.ip('me')
+        latitude, longitude = location.latlng
+        observation = owm.weather_manager().weather_at_coords(latitude, longitude)
         weather = observation.weather
         temperature = weather.temperature('celsius')['temp']
         status = weather.detailed_status
-        return f"The weather in {location} is {status} with a temperature of {temperature} degrees Celsius."
+        return f"The weather in {location.city} is {status} with a temperature of {temperature} degrees Celsius."
     except pyowm.exceptions.api_response_error.NotFoundError:
         return "Sorry, the city name was not found."
     except Exception as e:
@@ -86,10 +89,11 @@ def get_latest_news():
         top_headlines = newsapi.get_top_headlines(language='en', country='in')
         articles = top_headlines['articles']
         news_headlines = []
-        for idx, article in enumerate(articles, start=1):
+        for idx, article in enumerate(articles[:5], start=1):
+            source = article['source']['name']
             title = article['title']
-            description = article['description']
-            news_headlines.append((title, description))
+            # description = article['description']
+            news_headlines.append((source, title))
         return news_headlines
     except Exception as e:
         return f"An unexpected error occurred: {str(e)}"
@@ -97,19 +101,23 @@ def get_latest_news():
 
 def get_search_results(query):
     try:
-        vertexai.init(project="total-entity-409908", location="us-central1")
+        vertexai.init(project="seventhproject-418912", location="us-central1")
         parameters = {
             "candidate_count": 1,
             "max_output_tokens": 1024,
             "temperature": 0.9,
             "top_p": 1
         }
-        model = TextGenerationModel.from_pretrained("text-bison")
-        response = model.predict(
+        text_model = TextGenerationModel.from_pretrained("text-bison")
+        response = text_model.predict(
             query,
             **parameters
         )
-        return f"{response.text}"
+        lines = response.text.split('\n')[:2]
+
+    # Join the selected lines back together
+        selected_lines = '\n'.join(lines)
+        return f"Response from Model: {selected_lines}"
     except Exception as e:
         return f"An unexpected error occured: {str(e)}"
 
@@ -155,42 +163,73 @@ class VoiceAssistant:
         probs = F.softmax(outputs, dim=1)
         predicted_label = torch.argmax(probs).item()
         intents = ['time', 'weather', 'search', 'joke',
-                   'news', 'wikipedia']  # Update with your intents
+                   'news', 'wikipedia', 'play']  # Update with your intents
         return intents[predicted_label]
 
     def generate_response(self, intent, query):
         if intent == "time":
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             return f"The time is {strTime}"
+
         elif intent == "weather":
-            location = self.speak("Sure, please specify the city name.")
-            return get_weather(location)
+            # self.speak("Sure, please specify the city name.")
+            # location = self.take_command().lower()
+            return get_weather()
+
         elif intent == "news":
+            self.speak("Top 5 headlines are:")
             news = get_latest_news()
             if isinstance(news, list):
                 response = "Here are the latest news headlines:\n"
-                for idx, (title, description) in enumerate(news, start=1):
+                for idx, (source, title) in enumerate(news, start=1):
+                    response += f"According to {source}:"
                     response += f"Headline {idx}: {title}\n"
-                    response += f"Description: {description}\n"
+                    # response += f"Description: {description}\n"
                 return response
             else:
                 return news  # Handle error messages
+
         elif intent == "search":
+            pywhatkit.search(query)
             search_media = get_search_results(query)
             return search_media
+
+        elif intent == "wikipedia":
+            info = wikipedia.summary(query, 2)
+            # search_media = get_search_results(query)
+            return info
+
+        elif 'play' in query:
+            song = query.replace('play', "")
+            pywhatkit.playonyt(song)
+            return f"playing + {song}"
+
+        elif intent == 'joke':
+            return pyjokes.get_joke()
+
         else:
             return "How may I help you?"
 
     def start(self):
         self.speak("Initializing voice assistant...")
-        self.speak("Hello! I am your voice assistant.")
-        while True:
-            query = self.take_command().lower()
-            intent = self.classify_intent(query)
-            response = self.generate_response(intent, query)
-            self.speak(response)
+        self.speak("Hello! I am your voice assistant")
+        hour = int(datetime.datetime.now().hour)
+        if 0 <= hour < 12:
+            self.speak("Good Morning")
+        elif 12 <= hour < 18:
+            self.speak("Good Afternoon")
+        else:
+            self.speak("Good Evening")
+        self.speak("How may I help you?")
+        # while True:
+        query = self.take_command().lower()
+        intent = self.classify_intent(query)
+        response = self.generate_response(intent, query)
+        print(response)
+        self.speak(response)
 
 
 if __name__ == "__main__":
+
     assistant = VoiceAssistant()
     assistant.start()
